e50792b1b88ed2e5927fbf3822358179d3b5c858
Heshan Jayasinghe <heshan@Heshans-MacBook-Pro.local>
2021-05-11 08:30:56 +0800

    add auth server project

---
+++ src/test/java/com/hesh/auth/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hesh.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
