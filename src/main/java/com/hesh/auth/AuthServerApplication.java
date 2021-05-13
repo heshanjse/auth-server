b7c07a5e89ef95b2240025ca9d9922982d946909
Heshan Jayasinghe <heshan@Heshans-MacBook-Pro.local>
2021-05-13 20:03:20 +0800

    add spring security codes

--- src/main/java/com/hesh/auth/AuthServerApplication.java
+++ src/main/java/com/hesh/auth/AuthServerApplication.java
@@ -2,12 +2,21 @@ package com.hesh.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServerApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
